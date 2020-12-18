c4255dab385f22150c8e6668b1e83768216750d9
Sage Gonzales <sagegonzales15@gmail.com>
2020-12-17 21:55:46 -0700

    Add routes for crud actions

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :items do 
+        resources :merchants
+      end
+      namespace :merchants do
+        get '/find_all', to: 'search#find_all'
+        get '/most_revenue', to: 'search#most_revenue'
+      end 
+      resources :merchants do 
+        resources :items
+      end
+    end
+  end
+end
+
+# get '/api/v1/merchants/find_all', to: 'merchants/search#find_all'
